c622bba1ebe995a1ca61976f5379a8375786b4a0
mavvricks <150794652+mavvricks@users.noreply.github.com>
2024-06-30 18:53:34 +0800

    Create tsa8.js

---
+++ TSA8/tsa8.js
@@ -0,0 +1,42 @@
+function resetFlex() {
+    document.querySelector('.flex-container').style = '';
+    document.querySelectorAll('.box').forEach(box => {
+        box.style.flexGrow = '0';
+        box.style.width = '100px'; 
+        box.style.height = '100px'; 
+    });
+    document.getElementById('gap').value = 0;
+    document.getElementById('box1').value = 1;
+    document.getElementById('box2').value = 2;
+    document.getElementById('box3').value = 6;
+}
+
+function updateGap() {
+    const gapValue = document.getElementById('gap').value;
+    document.querySelector('.flex-container').style.gap = `${gapValue}px`;
+}
+
+function setDirection(direction) {
+    document.querySelector('.flex-container').style.flexDirection = direction;
+}
+
+function setJustify(justifyContent) {
+    document.querySelector('.flex-container').style.justifyContent = justifyContent;
+}
+
+function setAlign(alignItems) {
+    document.querySelector('.flex-container').style.alignItems = alignItems;
+}
+
+function resetGrow() {
+    document.querySelectorAll('.box').forEach(box => box.style.flexGrow = '0');
+}
+
+function growAll() {
+    document.querySelectorAll('.box').forEach(box => box.style.flexGrow = '1');
+}
+
+function updateGrow(boxNumber) {
+    const growValue = document.getElementById(`box${boxNumber}`).value;
+    document.getElementById(`box${boxNumber}`).style.flexGrow = growValue;
+}
