c7b852edfd6796a32356950a59fe7b45e517077c
mavvricks <150794652+mavvricks@users.noreply.github.com>
2024-07-15 20:13:41 +0800

    Add files via upload

---
+++ WebDes(FINALS)/Aquino_Duran_tsa8.js
@@ -0,0 +1,42 @@
+function resetFlex() {
+    document.querySelector('.flex-container').style = '';
+    document.querySelectorAll('.box').forEach(box => {
+        box.style.flexGrow = '0';
+        box.style.width = '100px';
+        box.style.height = '100px';
+    });
+    document.getElementById('gap').value = 0;
+    document.getElementById('grow1').value = 0;
+    document.getElementById('grow2').value = 0;
+    document.getElementById('grow3').value = 0;
+}
+
+function updateGap() {
+    const gapValue = document.getElementById('gap').value;
+    document.querySelector('.flex-container').style.gap = `${gapValue}px`;
+}
+
+function setDirection(direction) {
+    document.querySelector('.flex-container').style.flexDirection = direction;
+}
+
+function setJustify(justifyContent) {
+    document.querySelector('.flex-container').style.justifyContent = justifyContent;
+}
+
+function setAlign(alignItems) {
+    document.querySelector('.flex-container').style.alignItems = alignItems;
+}
+
+function resetGrow() {
+    document.querySelectorAll('.box').forEach(box => box.style.flexGrow = '0');
+}
+
+function growAll() {
+    document.querySelectorAll('.box').forEach(box => box.style.flexGrow = '1');
+}
+
+function updateGrow(boxNumber) {
+    const growValue = document.getElementById(`grow${boxNumber}`).value;
+    document.getElementById(`box${boxNumber}`).style.flexGrow = growValue;
+}
